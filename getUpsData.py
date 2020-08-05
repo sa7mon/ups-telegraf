c6790e24761a47548cbd38d080265db3db256a25
spidertyler2005 <38941820+spidertyler2005@users.noreply.github.com>
2020-08-05 15:25:27 -0400

    changed string measurements to just measurements
    string_measurements is now just measurements and it will autodetect if its a number or not! (UNTESTED!)

--- getUpsData.py
+++ getUpsData.py
@@ -2,7 +2,7 @@ import subprocess
 
 cmd="upsc ups"
 output=""
-string_measurements=["battery.mfr.date", "battery.type", "device.mfr", "device.model","device.serial","device.type",
+measurements=["battery.mfr.date", "battery.type", "device.mfr", "device.model","device.serial","device.type",
 			"driver.name", "driver.paramter.port", "driver.parameter.synchronous", "driver.version", "driver.version.data",
 			"ups.beeper.status", "ups.mfr","ups.model", "ups.serial", "ups.status", "ups.test.result", "driver.parameter.port",
 			"driver.parameter.syncronous"]
@@ -11,15 +11,16 @@ p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
 
 for line in p.stdout.readlines(): #read and store result in log file
     line = line.decode("utf-8").rstrip()
-    key = line[:line.find(":")]
-    value = line[line.find(":")+2:]
+    key = line.split(':')[0]
+    value = line.split(':')[1]
 
-    if key in string_measurements:
-        value = '"' + value + '"'
-    measurement = key + "=" + value
-    if output != "":
-        measurement = "," + measurement
-    output += measurement
+    if key in measurements:
+	try: float(value) #check if the value is a number
+	except: value = f'"{value}"' #adds quotes around the value string if value is not a number
+	measurement =f"{key}={value}"
+	if output != "":
+            measurement = "," + measurement
+	output += measurement
 
 output = "ups " + output.rstrip()
 print(output, end='')
